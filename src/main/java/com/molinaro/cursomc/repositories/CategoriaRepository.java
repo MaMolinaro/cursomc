787e1cd80f437235d433d8a5629b44e9cc2da98b
Marco Aurelio Molinaro <molinaromarcoaurelio@gmail.com>
2017-11-16 21:31:03 -0200

    Criando Repository e Service para Categoria

--- src/main/java/com/molinaro/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/molinaro/cursomc/repositories/CategoriaRepository.java
@@ -2,11 +2,12 @@ package com.molinaro.cursomc.repositories;
 
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.molinaro.cursomc.domain.Categoria;
 
 @Repository
-public class CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
 }
