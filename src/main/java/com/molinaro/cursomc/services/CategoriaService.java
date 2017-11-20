472e98f16dd9afa910b61d4eb0a3ba4a5a13f380
Marco Aurelio Molinaro <molinaromarcoaurelio@gmail.com>
2017-11-20 15:50:59 -0200

    Deletando com Categoria com DELETE. Implementando tratamento de exceção para Categoria com Produtos associados.

--- src/main/java/com/molinaro/cursomc/services/CategoriaService.java
+++ src/main/java/com/molinaro/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.molinaro.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.molinaro.cursomc.domain.Categoria;
 import com.molinaro.cursomc.repositories.CategoriaRepository;
+import com.molinaro.cursomc.services.exceptions.DataIntegrityException;
 import com.molinaro.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,5 +34,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);			
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir Categoria com Produtos associados!");
+		}
 	}
 }
